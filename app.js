5cdd9a5ff5e0845d2416932d38e787f7c90ae957
aziz-codes <azizisoffline@gmail.com>
2023-02-21 19:57:56 +0500

    array.find

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const array = ["b", "b", "c", "a", "d", "a"];
-const ob = array.includes("c", 3);
+const ob = array.find((item) => item === "a");
 console.log(ob);
-//search for an element in array if found returns true else false.
-// param: i. element ii. index -> optional
+
+//It returns the first occured value which satisfies the provided testing function else retuns undefined.
