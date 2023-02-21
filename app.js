f2d45102f468a40a77e7523062e3a4f3de122414
aziz-codes <azizisoffline@gmail.com>
2023-02-21 19:45:07 +0500

    array.slice

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const array = ["a", "b", "c", "d"];
+const ob = array.slice(-4, -3);
+console.log(ob);
 
-console.log(array.slice(0, 3));
+//array.slice make a shallow copy of input array and returns a new array.
+//if the start index is negative, => start+array.length.
+// the same goes for end param.
