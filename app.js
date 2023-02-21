411ed5152839dc2a16088e22d56357c4cedd3662
aziz-codes <azizisoffline@gmail.com>
2023-02-21 19:59:40 +0500

    array.findIndex

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const array = ["b", "b", "c", "a", "d", "a"];
-const ob = array.find((item) => item === "a");
+const ob = array.findIndex((item) => item === "ff");
 console.log(ob);
 
-//It returns the first occured value which satisfies the provided testing function else retuns undefined.
+//It returns the first occured value index which satisfies the provided testing function else retuns -1.
